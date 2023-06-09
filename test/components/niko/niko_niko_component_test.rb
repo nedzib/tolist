3f2cc4301e1ebcaa4baf45a3ea12141c97908bf3
Nedzib Sastoque <bsastoque@buk.co>
2023-06-08 21:00:15 -0500

    feat: components generatedd

---
+++ test/components/niko/niko_niko_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Niko::NikoNikoComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Niko::NikoNikoComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
---
+++ test/components/task_list/task_list_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TaskList::TaskListComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(TaskList::TaskListComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
