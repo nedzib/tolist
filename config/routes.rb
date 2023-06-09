9a49cb0c55eb942179d1b820a74594e32fcc0ea6
Nedzib Sastoque <bsastoque@buk.co>
2023-06-08 20:52:34 -0500

    feat: dashboard view added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'landing/index'
+  get '/dashboard', to: "dashboard#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
